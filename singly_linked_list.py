5ffbfaaa048e47f2d4b0bdf1272f9eb853b92cc3
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-23 00:38:59 +0600

    singly linked list code done

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -21,3 +21,53 @@ class LinkedList:
         while current.next is not None:
             current = current.next
         current.next = new_node
+
+    def insert_after(self, prev_node, data):
+        if prev_node is None:
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def insert_before(self, next_node, data):
+        if next_node is None:
+            return
+        new_node = Node(data)
+        new_node.next = next_node
+        prev_node = self.head
+        while prev_node.next is not next_node:
+            prev_node = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key):
+        current = self.head
+        if current is not None:
+            if current.data == key:
+                self.head = current.next
+                current = None
+                return
+        while current is not None:
+            if current.data == key:
+                break
+            prev = current
+            current = current.next
+        if current is None:
+            return
+        prev.next = current.next
+        current = None
+
+    def print_list(self):
+        current = self.head
+        while current is not None:
+            print(current.data)
+            current = current.next
+
+llist = LinkedList()
+llist.insert_at_beginning(1)
+llist.insert_at_beginning(2)
+llist.insert_at_beginning(3)
+llist.insert_at_end(4)
+llist.insert_after(llist.head.next, 5)
+llist.insert_before(llist.head.next.next, 6)
+llist.delete_node(3)
+llist.print_list()
