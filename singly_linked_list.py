8deb0485075e6690015eccb0f8493d5056e5714c
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-23 00:30:50 +0600

    insert at beginning function in simply linked list

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -5,3 +5,9 @@ class Node:
 
 class LinkedList:
     def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node 
