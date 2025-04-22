1a2b34ba8a0ca521c9c572aec83a5daa03f5d8ef
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-23 00:32:10 +0600

    insert at end data module function done

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -10,4 +10,14 @@ class LinkedList:
     def insert_at_beginning(self, data):
         new_node = Node(data)
         new_node.next = self.head
-        self.head = new_node 
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = new_node
