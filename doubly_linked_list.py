cb5a60765ac07b9ea0f7e0c334eb854ed4c5689b
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-23 01:04:10 +0600

    insert at beginning function done

---
+++ doubly_linked_list.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
