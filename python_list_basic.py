dc06d3af53fbf8bd8b7e409a5754b0d6b8638c87
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-23 22:09:14 +0600

    python list clarification

---
+++ python_list_basic.py
@@ -0,0 +1,20 @@
+thisList = ['apple', 'banana', 'cherry', 'apple', 'cherry']
+print(thisList)
+
+thisList = ['apple', 'banana', 'cherry']
+print(len(thisList))
+
+thisList = ['apple', 'banana', 'cherry']
+print(thisList[1])
+
+thisList = ['apple', 'banana', 'cherry']
+print(thisList[-1])
+
+thisList = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+print(thisList[2:5])
+
+thisList = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+print(thisList[:4])
+
+thisList = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+print(thisList[2:])
