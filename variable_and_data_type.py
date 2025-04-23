b37c8becefc2e64e74ba98b7d3db1899b79bfa96
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-24 00:57:28 +0600

    two input number summation here

--- variable_and_data_type.py
+++ variable_and_data_type.py
@@ -15,3 +15,8 @@ print(type(name))
 print(type(age))
 print(type(height))
 print(type(is_married))
+
+number1 = (input("Enter a number: "))
+number2 = (input("Enter another number: "))
+
+print("The sum of", number1, "and", number2, "is", int(number1) + int(number2))
