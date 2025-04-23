4a2ec0a3c9d1481c094eec4a571222ccd05d31bc
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-24 01:11:40 +0600

    traffic condition

---
+++ traffic_condition.py
@@ -0,0 +1,12 @@
+ligth = "green"
+
+if(ligth == "green"):
+    print("you can go")
+elif(ligth == "yellow"):
+    print("wait")
+elif(ligth == "red"):
+    print("stop")
+else:
+    print("wrong input")
+
+print('End of code')
