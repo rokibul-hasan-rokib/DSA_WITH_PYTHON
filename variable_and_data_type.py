a6b8cb082b970d7b28a71beb976c35dc599d87b2
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-24 00:52:29 +0600

    variable declare and type declare also

---
+++ variable_and_data_type.py
@@ -0,0 +1,17 @@
+name = "Rokibul Hasan Rokib"
+
+age = 20
+
+height = 5.9
+
+is_married = False  
+
+print(name)
+print(age)
+print(height)
+print(is_married)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_married))
