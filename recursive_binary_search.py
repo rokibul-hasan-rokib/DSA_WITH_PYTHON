64fa3b94ff5664a3eb97c702b193d0c579957170
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-28 00:23:00 +0600

    recursive comment out

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -1,17 +1,16 @@
 def binary_search_recursive(arr, target, left, right):
     if left > right:
-        return -1  # Target not found
+        return -1  
     
     mid = (left + right) // 2
     
     if arr[mid] == target:
-        return mid  # Target found
+        return mid  
     elif arr[mid] < target:
         return binary_search_recursive(arr, target, mid + 1, right)
     else:
         return binary_search_recursive(arr, target, left, mid - 1)
 
-# Example usage:
 arr = [2, 4, 6, 8, 10, 12, 14]
 target = 10
 result = binary_search_recursive(arr, target, 0, len(arr) - 1)
