cb0c5a093af5533cc8df8a33e13518477da4cc2a
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-04-29 21:35:02 +0600

    recursive binary

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -10,8 +10,10 @@ def binary_search_recursive(arr, target, left, right):
         return binary_search_recursive(arr, target, mid + 1, right)
     else:
         return binary_search_recursive(arr, target, left, mid - 1)
-
+    
 arr = [2, 4, 6, 8, 10, 12, 14]
 target = 10
 result = binary_search_recursive(arr, target, 0, len(arr) - 1)
+
+
 print(f"Target found at index: {result}" if result != -1 else "Target not found")
