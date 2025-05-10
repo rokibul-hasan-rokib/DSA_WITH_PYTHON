4c932eafaaf7c1832af6818855af9387a44a5004
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-10 23:35:49 +0600

    fix: add initial implementation of dictionary access and print statements

---
+++ dictionary_access_items.py
@@ -0,0 +1,16 @@
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+x = thisdict["model"]
+print(x)  # Output: Mustang
+
+
+x = thisdict.get("model")
+print(x)  # Output: Mustang
+
+x = thisdict.keys()
+print(x)
+
