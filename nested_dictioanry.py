cc83a6a521fe8847747401a66086377f1291a848
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-10 23:50:37 +0600

    fix: add initial implementation of nested dictionary with print statements

---
+++ nested_dictioanry.py
@@ -0,0 +1,30 @@
+myfamily = {
+    "child1": {
+        "name": "Emil",
+        "year": 2004
+    },
+    "child2": {
+        "name": "Tobias",
+        "year": 2007
+    },
+    "child3": {
+        "name": "Linus",
+        "year": 2011
+    }
+}
+
+
+myfamily = {    
+    "child1": child1,    
+    "child2": child2,    
+    "child3": child3
+}
+
+for x, obj in family.items():
+    print(x)
+    print(obj["name"])
+    print(obj["year"])
+
+    for y in obj:
+        print(y)
+    # Output: Emil
