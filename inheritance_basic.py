dd65161bc7c8f05289ae97de411b09c7d41dbc00
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-16 23:34:20 +0600

    feat: add Student class with initialization and welcome method

--- inheritance_basic.py
+++ inheritance_basic.py
@@ -10,3 +10,16 @@ class Person:
 x = Person("John", "Doe", 36)
 y = Person("Jane", "Doe", 28)
 x.printname()
+
+class Student(Person):
+    def __init__(self, fname, lname, age):
+        Person.__init__(self, fname, lname, age)
+        self.graduationyear = 2023
+
+    def welcome(self):
+        print("Welcome " + self.firstname + " " + self.lastname + " to the class of " + str(self.graduationyear))
+
+x = Student("John", "Doe", 36)
+y = Student("Jane", "Doe", 28)
+x.welcome()
+y.welcome()
