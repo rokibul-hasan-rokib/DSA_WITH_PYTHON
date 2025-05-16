709715430a5f730c3acd8c1aaf1143a7e87b27ea
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-16 23:44:51 +0600

    feat: update myRokib function and add variable scope examples

--- scope_basic.py
+++ scope_basic.py
@@ -4,3 +4,16 @@ def myFunc():
 
 myFunc()
 # print(x) # This will raise an error because x is not defined in this scope
+
+x = 100000
+
+def myRokib():
+    x = 10
+    print(x)
+
+myRokib()
+# print(x) # This will raise an error because x is not defined in this scope
+# x = 100000
+def myRokib():
+    x = 10
+    print(x)
