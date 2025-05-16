f1ae668f27ec779d76117aa3891ac2fb306aa563
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-16 23:57:05 +0600

    feat: add additional JSON serialization examples in python_json.py

--- python_json.py
+++ python_json.py
@@ -10,3 +10,13 @@ x = {
 y = json.dumps(x)
 
 print(y)
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
