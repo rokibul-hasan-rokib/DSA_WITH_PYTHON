73404856acc255fb006e95cfb4d8b46ffced8aa5
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-16 23:37:11 +0600

    feat: add MyNumbers class with iterator implementation

--- iterators_basic.py
+++ iterators_basic.py
@@ -21,3 +21,26 @@ print(next(myit))  # country
 
 for x in mytuple:
     print(x)  # apple, banana, cherry
+
+
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        if self.a < 10:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+        
+myclass = MyNumbers()
+myiter = iter(myclass)
+for x in myiter:
+    print(x)  # 1, 2, 3, 4, 5, 6, 7, 8, 9
+# StopIteration exception will be raised if there are no more items
+myclass = MyNumbers()
+myiter = iter(myclass)
+print(next(myiter))
