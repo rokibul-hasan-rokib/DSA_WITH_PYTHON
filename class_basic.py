ead469bd4bfb194a1c6de8895def18e8aaed28bd
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-16 22:43:15 +0600

    feat: implement Person class with attributes and instance creation

--- class_basic.py
+++ class_basic.py
@@ -3,3 +3,16 @@ class MyClass:
 
 p1 = MyClass()
 print(p1.x)
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("John", 36)
+p2 = Person("Jane", 28)
+print(p1.name)
+print(p2.age)
+print(p1.age)
+print(p2.name)
