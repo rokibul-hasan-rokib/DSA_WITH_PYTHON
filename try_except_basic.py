2ec1565b6ae4e9e91e8d3ad9b6d5f6846b5f108c
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-05-18 10:07:36 +0600

    feat: else how work

--- try_except_basic.py
+++ try_except_basic.py
@@ -9,3 +9,10 @@ except NameError:
   print("Variable x is not defined")
 except:
   print("Something else went wrong")
+  
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong") 
