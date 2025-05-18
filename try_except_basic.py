4658c5aa1c1c43f5304cd1a36c3fe5493aabdc6b
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-05-18 10:08:08 +0600

    feat: finally function how work here see this

--- try_except_basic.py
+++ try_except_basic.py
@@ -16,3 +16,11 @@ except:
   print("Something went wrong")
 else:
   print("Nothing went wrong") 
+  
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+  
