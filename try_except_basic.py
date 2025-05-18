714bbb3b05d79b53f827ab79cf77097aa9893503
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-05-18 10:06:55 +0600

    feat: Variable NameError added

--- try_except_basic.py
+++ try_except_basic.py
@@ -2,3 +2,10 @@ try:
     print(x)
 except:
     print("An exception occurred")
+    
+try:
+  print(y)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
