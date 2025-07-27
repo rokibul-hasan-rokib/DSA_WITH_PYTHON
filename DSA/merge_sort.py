2c254652a1e4f6f713683713ac2ca33635b678a8
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-07-27 09:15:36 +0600

    feat: merge sort

---
+++ DSA/merge_sort.py
@@ -0,0 +1,38 @@
+def merge(left, right):
+  result = []
+  i = j = 0
+
+  while i < len(left) and j < len(right):
+    if left[i] < right[j]:
+      result.append(left[i])
+      i += 1
+    else:
+      result.append(right[j])
+      j += 1
+
+  result.extend(left[i:])
+  result.extend(right[j:])
+
+  return result
+
+def mergeSort(arr):
+  step = 1 
+  length = len(arr)
+
+  while step < length:
+    for i in range(0, length, 2 * step):
+      left = arr[i:i + step]
+      right = arr[i + step:i + 2 * step]
+
+    merged = merge(left, right)
+
+    for j, val in enumerate(merged):
+      arr[i + j] = val
+
+    step *= 2 
+
+  return arr
+
+mylist = [3, 7, 6, -10, 15, 23.5, 55, -13]
+mysortedlist = mergeSort(mylist)
+print(mysortedlist)
